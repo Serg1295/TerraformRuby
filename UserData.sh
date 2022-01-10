756fbafd650ad00e4c12f32d36a6fa3d0aefbed6
Sergo Hovakimyan <sergo.hovakimyan.12@gmail.com>
2022-01-10 17:21:55 +0400

    version 1.0.1

--- UserData.sh
+++ UserData.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+sudo apt-get update
 sudo apt-get install curl
 sudo apt install gnupg
 curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
@@ -10,3 +11,4 @@ rvm requirements
 rvm install ruby-2.6.6
 rvm use 2.6.6 --default
 gem install rails --version=5.2.3
+sudo apt-get install nodejs
--- main.tf
+++ main.tf
@@ -10,6 +10,20 @@ resource "aws_vpc" "VPC" {
     Name = "VPC_MAIN"
   }
 }
+resource "aws_default_vpc" "Default_VPC" {
+  tags = {
+    Name = "Default_VPC"
+  }
+}
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+}
+resource "aws_default_subnet" "default_az3" {
+  availability_zone = data.aws_availability_zones.available.names[2]
+}
 resource "aws_internet_gateway" "IGW" { #--internet getaway for vpc
   vpc_id = aws_vpc.VPC.id
 
@@ -80,12 +94,20 @@ resource "aws_route_table" "VPC_Public_Route" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.IGW.id
   }
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.Peer_to_Def.id
+  }
   tags = {
     Name = "VPC_Public_Route"
   }
 }
 resource "aws_route_table" "VPC_Private_Route" {
   vpc_id = aws_vpc.VPC.id
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.Peer_to_Def.id
+  }
   tags = {
     Name = "VPC_Private_Route"
   }
@@ -114,13 +136,37 @@ resource "aws_route_table_association" "Association_Public_C" {
   subnet_id      = aws_subnet.Public-C.id
   route_table_id = aws_route_table.VPC_Public_Route.id
 }
+resource "aws_route_table_association" "Association_Default_az1" {
+  subnet_id      = aws_default_subnet.default_az1.id
+  route_table_id = aws_default_vpc.Default_VPC.default_route_table_id
+}
+resource "aws_route_table_association" "Association_Default_az2" {
+  subnet_id      = aws_default_subnet.default_az2.id
+  route_table_id = aws_default_vpc.Default_VPC.default_route_table_id
+}
+resource "aws_route_table_association" "Association_Default_az3" {
+  subnet_id      = aws_default_subnet.default_az3.id
+  route_table_id = aws_default_vpc.Default_VPC.default_route_table_id
+}
+#---Vpc peering VPC_MAIN -> default VPC ----------------------
+resource "aws_vpc_peering_connection" "Peer_to_Def" {
+  peer_vpc_id = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.VPC.id
+  auto_accept = true
+}
+
+resource "aws_route" "Default_Rout" {
+  route_table_id            = aws_default_vpc.Default_VPC.default_route_table_id
+  destination_cidr_block    = aws_vpc.VPC.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.Peer_to_Def.id
+}
 #-------------------------------------------------------------------
 resource "aws_security_group" "SG_EC2_Ruby" {
   name   = "SG_EC2_Ruby"
   vpc_id = aws_vpc.VPC.id
 
   dynamic "ingress" {
-    for_each = ["80", "22"]
+    for_each = ["3000", "22"]
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -144,9 +190,54 @@ resource "aws_instance" "EC2_Ruby" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.Public-A.id
   vpc_security_group_ids = [aws_security_group.SG_EC2_Ruby.id]
+  private_ip             = cidrhost(aws_subnet.Public-A.cidr_block, 6)
   key_name               = data.aws_key_pair.Frankfurt_key.key_name
   user_data              = file("UserData.sh")
   tags = {
     Name = "EC2_Ruby"
   }
+}
+#----------------------------------------------------------------------
+resource "aws_db_subnet_group" "DB_Subnet_Group" {
+  name       = "DB_Subnet_Group"
+  subnet_ids = [aws_subnet.Private-A.id, aws_subnet.Private-B.id, aws_subnet.Public-C.id]
+
+  tags = {
+    Name = "DB_subnet_group"
+  }
+}
+resource "aws_security_group" "SG_DB_MySQL" {
+  name   = "SG_DB_MySQl"
+  vpc_id = aws_vpc.VPC.id
+
+  dynamic "ingress" {
+    for_each = ["3306"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = aws_vpc.VPC.cidr_block
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "SG_DB_MySQL"
+  }
+}
+resource "aws_db_instance" "MySQL_DB" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  name                   = "mydb"
+  username               = "foo"
+  password               = "foobarbaz"
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.DB_Subnet_Group.name
+  vpc_security_group_ids = [aws_security_group.SG_DB_MySQL.id]
 }
